072074f18fe1e3f100c95a131a82b45cc505706a
cauelt <cauelt@3d79d307-c547-0410-a3ea-3bdc3fcf0428>
2008-04-04 00:31:31 +0000

    [artigo19] Agora sim adicionando admin

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<?
+	require_once("includes/phpmyedit-conf.php");
+	header('Content-Type: text/html; charset=iso-8859-1');
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Mapa das r&aacute;dios comunit&aacute;rias - Gerenciamento</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link href="css/geral.css" rel="stylesheet" type="text/css">
+<link href="css/phpmyedit.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="corpo">
+	<h1>Mapa das r&aacute;dios comunit&aacute;rias</h1>
+	<?php new phpMyEdit($opts); ?>
+	</div>
+</body>
+</html>
