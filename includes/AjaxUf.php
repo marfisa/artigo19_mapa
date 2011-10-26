92369e939f9a5f60cd4875233b8e3b70454db162
Rodrigo Primo <rodrigosprimo@gmail.com>
2011-10-26 09:38:47 -0200

    indentacao

--- includes/AjaxUf.php
+++ includes/AjaxUf.php
@@ -1,30 +1,31 @@
 <?
-	require_once('utils.inc.php');
-	header("Content-Type: text/plain; charset=UTF-8");
+require_once('utils.inc.php');
+header("Content-Type: text/plain; charset=UTF-8");
 
-	if (isset($_GET['UF'])) $uf = $_GET['UF'];
-	else $uf = null;
+if (isset($_GET['UF'])) {
+	$uf = $_GET['UF'];
+} else {
+	$uf = null;
+}
 	
-	if ($uf != NULL)
-	{
-		$resMunicipios = query(sprintf($sqlGetMunicipiosByCodUf,$uf));
+if ($uf != NULL) {
+	$resMunicipios = query(sprintf($sqlGetMunicipiosByCodUf,$uf));
 
-    $jsonOutput .= '    [' . "\r\n";
+	$jsonOutput .= '    [' . "\r\n";
 
-		while ($myMunicipio = mysql_fetch_array($resMunicipios))
-		{
-      $jsonOutput .= '      {' . "\r\n";
-      $jsonOutput .= '        "cod": "' . utf8_encode($myMunicipio['codigo']) . '",' . "\r\n";
-      $jsonOutput .= '        "nome": "' . utf8_encode($myMunicipio['nome']) . '"' . "\r\n";
-      $jsonOutput .= '      },' . "\r\n";
-		}
-		
-		$jsonOutput = substr($jsonOutput, 0, -3);
-		$jsonOutput .= "\r\n";
-
-    $jsonOutput .= '    ]' . "\r\n";
-
-    echo($jsonOutput);
-
+	while ($myMunicipio = mysql_fetch_array($resMunicipios)) {
+		$jsonOutput .= '      {' . "\r\n";
+		$jsonOutput .= '        "cod": "' . utf8_encode($myMunicipio['codigo']) . '",' . "\r\n";
+		$jsonOutput .= '        "nome": "' . utf8_encode($myMunicipio['nome']) . '"' . "\r\n";
+		$jsonOutput .= '      },' . "\r\n";
 	}
+		
+	$jsonOutput = substr($jsonOutput, 0, -3);
+	$jsonOutput .= "\r\n";
+
+	$jsonOutput .= '    ]' . "\r\n";
+
+	echo($jsonOutput);
+
+}
 ?>
